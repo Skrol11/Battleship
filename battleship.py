f0a24098e2cf14ca8db02373019be88df6b09fce
Skrol11 <krolas94@gmail.com>
2020-09-08 19:02:52 +0200

    Instrucion the game

--- battleship.py
+++ battleship.py
@@ -1,5 +1,28 @@
-# main
+#1 def init_board(board) 5x5 lista list
 
-# get move
 
-# has_won
+#2 print boarda col and row 
+
+#3 Rozmieszczanie statków po boardzie
+#while
+# get_init_ship sprawdza poprawność wpisania col and row isalpha isnumeric, " ", input result
+    # jeżeli jest to nadpisujemy boarda X
+    #       def can_init_ship() --> return TRUE na start, sprawdzanie czy mozna postawic statek
+    #       A1 --> X board[result[0]][result[1]]
+    # jeżeli nie to zwraca nam 3 
+#4 def can_init_ship()
+#5 print_board(board)
+#6 move #3, move #4, move #5
+#7 start_game()
+#8 shooting_phase():
+    #sprawdzanie czy ktoś trafił czy nie
+    #   jeżeli tak to nadpisujemy boarda H
+        # jeżeli zatopił S
+    #   jeżeli nie trafił to M
+#9 has_won() True or False
+#10 print_board()
+#11 Zapętlić program aż ktoś wygra
+#12 Opcjonajlnie menu dla 1vs1 lub 1vs AI
+
+    
+
